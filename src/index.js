22197a9f8d68cfae42142435d0064fdb54fa7b91
Vick Scarlet <scarlet_vick@outlook.com>
2021-09-15 10:33:45 +0800

    use commander

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+#!/usr/bin/env node
+import commander from 'commander';
+import { readFile } from 'fs/promises';
 import { readXLSX, writeJSON, jobs as getJobs } from './io.js';
 import { jobs as doJobs } from './transform/index.js';
 
-async function main() {
-    await doJobs(
-        await getJobs(
-            process
-                .argv
-                .slice(2)
-        ),
-        readXLSX,
-        writeJSON,
-    );
-}
+commander
+    .command('version')
+    .action(async () => {
+        console.info(
+            JSON.parse(
+                    await readFile('package.json')
+                )
+                .version
+        );
+    });
 
-main();
+commander
+    .command('transform [list...]')
+    .action(async list => {
+        await doJobs(
+            await getJobs(list),
+            readXLSX,
+            writeJSON,
+        );
+    });
+
+commander
+    .name('vt');
+
+commander.parse(process.argv);
