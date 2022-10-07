00bfea9b03f3b6c820de575ea0053ba751909f82
Vick Scarlet <scarlet_vick@outlook.com>
2022-10-07 13:42:27 +0800

    version: 2.0.0

---
+++ src/transform.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import { load } from './loader.js';
+import { prepare } from './prepare.js';
+import { parser } from './parser.js';
+import { dump } from './dump.js';
+
+export async function transform(options) {
+    const now = Date.now();
+    const configurations = await load(options);
+    for(const config of configurations)
+        await task(config);
+    console.info(`Transformed in ${Date.now() - now}ms`);
+}
+
+async function task({files, dest, cwd, type, space}) {
+    console.info('Transform task config:', {files, dest, cwd, type, space});
+    const m = new Map();
+    for(const file of files) {
+        const dir = path.resolve(dest, path.dirname(file));
+        for(const {name, data} of (await prepare(path.resolve(cwd, file)))) {
+            if(name[0] === "#") continue;
+            let sheet = path.resolve(dir, name.split('#')[0]);
+            if(sheet[0] === '>') sheet = sheet.substring(1)
+            sheet = sheet.replace('<arr>', '');
+            if(!m.has(sheet))
+                m.set(sheet, new JobData());
+            m.get(sheet).append(parser(data));
+        }
+    }
+    for(const [sheet, data] of m)
+        await dump(sheet, data.result(), type, space);
+}
+
+class JobData {
+    constructor(data) {
+        if(data) this.append(data);
+    }
+
+    #data = [];
+
+    append(data) {
+        this.#data.push(data);
+    }
+
+    result() {
+        if(!this.#data.length) return {};
+        const data = this.#data;
+        let result;
+        if(Array.isArray(data[0])) {
+            result = [];
+            for(const subs of data)
+                result.push(...Object.values(subs));
+        } else {
+            result = {};
+            for(const subs of data)
+                Object.assign(result, subs);
+        }
+        return result;
+    }
+}
--- src/transform/merge.js
+++
@@ -1,6 +0,0 @@
-export function merge(datas) {
-    if(Array.isArray(datas[0]))
-        return datas.flat();
-
-    return Object.assign({}, ...datas);
-}
